769152218612e96e92d9c32932303cebec35ccdb
Ghosttrio <ghosttrio@naver.com>
2024-12-02 11:23:25 +0900

    init: first commit

---
+++ src/main/kotlin/com/ghosttrio/gilbadak/GilbadakApplication.kt
@@ -0,0 +1,11 @@
+package com.ghosttrio.gilbadak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GilbadakApplication
+
+fun main(args: Array<String>) {
+	runApplication<GilbadakApplication>(*args)
+}
---
+++ src/test/kotlin/com/ghosttrio/gilbadak/GilbadakApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghosttrio.gilbadak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GilbadakApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
