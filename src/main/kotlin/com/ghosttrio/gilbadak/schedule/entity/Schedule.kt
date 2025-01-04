853a02c56a2b07f075ed5acb253ad46ba76f4868
Ghosttrio <ghosttrio@naver.com>
2025-01-04 11:50:36 +0900

    chore: docker compose 파일 추가, 데이터베이스 연결

--- src/main/kotlin/com/ghosttrio/gilbadak/schedule/entity/Schedule.kt
+++ src/main/kotlin/com/ghosttrio/gilbadak/schedule/entity/Schedule.kt
@@ -1,4 +1,16 @@
 package com.ghosttrio.gilbadak.schedule.entity
 
-class Schedule {
+import com.ghosttrio.gilbadak.util.BaseTimeEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.IDENTITY
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "tb_schedules")
+class Schedule(
+    @Id @GeneratedValue(strategy = IDENTITY)
+    val id: Long,
+) : BaseTimeEntity() {
 }
